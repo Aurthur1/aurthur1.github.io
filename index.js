6fc9aef3e60bf0c2b84248cdec820b92b3c96a43
Aurthur1 <yunusasunny2@gmail.com>
2020-08-11 21:15:04 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('.btn');
 
 btn.onclick = () => {
-  alert('Boo Boo come here for some tranquilizing moment!');
+  alert('Boo Boo come here for some tranquilizing  moment!');
 };
